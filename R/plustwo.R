b575fa1761cd5575dc7da40f298b4d0995639bcf
Sihao Miao <simiao@uw.edu>
2020-03-02 21:20:04 -0800

    update

---
+++ R/plustwo.R
@@ -0,0 +1,12 @@
+#' Plus Two Function
+#'
+#' @param x
+#' a numeric number
+#' @return the input number plus two
+#' @export
+#'
+#' @examples
+#' plustwo(2)
+plustwo <- function(x){
+  x+2
+}
---
+++ R/square.R
@@ -0,0 +1,13 @@
+#' Square Function
+#'
+#' @param x
+#' a numeric number
+#' @return the square value of the imput number
+#' @export
+#'
+#' @examples
+#' square(2)
+square <- function(x) {
+
+    return(x^2)
+}
